b1b91ad9c24b72d582a571808a5323114966bd32
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-11-07 15:36:16 -0300

    07/11

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -10,7 +10,6 @@ function loginRedirect() {
 }
 
 //====================================MENU====================================\\
-document.getElementById('arrow');
 document.getElementById('logo');
 
 // EVENT LISTENERS
@@ -20,3 +19,34 @@ logo.addEventListener('click', homeRedirect)
 function homeRedirect() {
     window.location.href = '../index.html';
 }
+
+//====================================SIDE-MENU====================================\\
+const sideMenu = document.getElementById('sideMenu');
+const arrow = document.getElementById('arrow');
+var cSideMenu = 0;
+let isRotated = false; 
+
+//EVENT LISTENERS
+arrow.addEventListener('click', showSideMenu);
+
+//FUNCTIONS
+function showSideMenu() {
+    if (cSideMenu == 0){
+        sideMenu.style.display='flex';
+        cSideMenu = 1;
+    } else{
+        sideMenu.style.display='none';
+        cSideMenu = 0;
+    }
+}
+
+
+arrow.addEventListener("click", () => {
+    if (isRotated) {
+        arrow.style.transform = "rotate(deg)";
+        isRotated=false;
+    } else {
+        arrow.style.transform = "rotate(90deg)";
+        isRotated=true;
+    }
+});
