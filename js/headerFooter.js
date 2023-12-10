eb534515e0ab1f900b4d6768fe4a2379561e9718
paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-12-09 22:15:33 -0300

    Páginas de login e cadastro finalizadas. Página de ofertas finalizada. Páginas de produtos faltando apenas integração dos carrosseis (página de produtos de cachorros já está finalizada)

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,19 +1,12 @@
-//====================================LOGIN-BUTTON====================================\\
-const login = document.getElementById('login');
-
-// EVENT LISTENERS
-login.addEventListener('click', loginRedirect);
-
-// FUNCTIONS
-function loginRedirect() {
-    window.location.href = '../html/login.html';
-}
-
 //====================================MENU====================================\\
-document.getElementById('logo');
+var logo = document.getElementById('logo');
 
 // EVENT LISTENERS
-logo.addEventListener('click', homeRedirect)
+logo.addEventListener('click', function () {
+    if (!window.location.pathname.endsWith('/index.html')) {
+        homeRedirect();
+    }
+});
 
 // FUNCTIONS
 function homeRedirect() {
@@ -24,18 +17,18 @@ function homeRedirect() {
 const sideMenu = document.getElementById('sideMenu');
 const arrow = document.getElementById('arrow');
 var cSideMenu = 0;
-let isRotated = false; 
+let isRotated = false;
 
 //EVENT LISTENERS
 arrow.addEventListener('click', showSideMenu);
 
 //FUNCTIONS
 function showSideMenu() {
-    if (cSideMenu == 0){
-        sideMenu.style.display='flex';
+    if (cSideMenu == 0) {
+        sideMenu.style.display = 'flex';
         cSideMenu = 1;
-    } else{
-        sideMenu.style.display='none';
+    } else {
+        sideMenu.style.display = 'none';
         cSideMenu = 0;
     }
 }
@@ -44,9 +37,9 @@ function showSideMenu() {
 arrow.addEventListener("click", () => {
     if (isRotated) {
         arrow.style.transform = "rotate(0deg)";
-        isRotated=false;
+        isRotated = false;
     } else {
         arrow.style.transform = "rotate(90deg)";
-        isRotated=true;
+        isRotated = true;
     }
 });
